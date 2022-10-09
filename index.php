3ebaa8f3105778d06767604fbb5d6c88a52b0de2
Natalya Babenko <natapervomaisk88@gmail.com>
2022-10-09 16:31:26 +0300

    changed require autoload

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-namespace App;
 require_once __DIR__ . '/vendor/autoload.php';
 use App\Student;
 use App\Employee;
@@ -12,8 +10,4 @@ echo $student->getCourse() . "<br>";
 $empl1 = new Employee("eric", 25, 1000);
 $empl2 = new Employee("kyle", 30, 2000);
 echo "Salary: " . $empl1->getSalary();
-
-
-
-
 ?>
